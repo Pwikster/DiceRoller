7688619b482105ac030d2676c11945dca38d03d0
Patrick <pwkjr1@gmail.com>
2024-12-08 13:32:36 -0800

    practicing modularizing widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:practice/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(),
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  @override //used to ensure that we are not using any methods incorrectly.
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 32, 32, 32),
-            Color.fromARGB(255, 48, 48, 48),
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Howdy Partner.',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 32,
-          ),
-        ),
-      ),
-    );
-  }
 }
