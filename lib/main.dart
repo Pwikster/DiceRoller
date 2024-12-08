28cd1e0c096de45c3e950ad263a609337749feab
Patrick <pwkjr1@gmail.com>
2024-12-08 13:14:27 -0800

    Practicing making custom widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,35 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 32, 32, 32),
-                Color.fromARGB(255, 48, 48, 48),
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Howdy Partner.',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 32,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override //used to ensure that we are not using any methods incorrectly.
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 32, 32, 32),
+            Color.fromARGB(255, 48, 48, 48),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Howdy Partner.',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+          ),
+        ),
+      ),
+    );
+  }
 }
