38aa786e331db66a97352facfebbf5e8b156976a
Patrick <pwkjr1@gmail.com>
2024-12-08 12:52:08 -0800

    Practicing styling

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,24 @@ void main() {
     MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
                 Color.fromARGB(255, 32, 32, 32),
-                Color.fromARGB(255, 64, 64, 64),
+                Color.fromARGB(255, 48, 48, 48),
               ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
             ),
           ),
           child: const Center(
-            child: Text('Howdy'),
+            child: Text(
+              'Howdy Partner.',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+              ),
+            ),
           ),
         ),
       ),
