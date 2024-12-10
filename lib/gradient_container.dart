a31c28bcefee00942ced85b90b4b1508d2f67018
Patrick <pwkjr1@gmail.com>
2024-12-10 05:53:37 -0800

    Practicing buttons

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -6,6 +6,8 @@ class GradientContainer extends StatelessWidget {
 
   final List<Color> colors;
 
+  void rollDice() {}
+
   @override //used to ensure that we are not using any methods incorrectly.
   Widget build(context) {
     return Container(
@@ -16,9 +18,31 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomCenter,
         ),
       ),
-      child:  Center(
-        child: Image.asset('assets/images/dice-1.png'),
-      ),
+      child: Center(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/dice-1.png',
+            width: 200,
+          ),
+          TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+              padding: const EdgeInsets.all(
+                20,
+              ),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(
+                fontSize: 32,
+              ),
+            ),
+            child: const Text(
+              'Roll Dice',
+            ),
+          )
+        ],
+      )),
     );
   }
 }
